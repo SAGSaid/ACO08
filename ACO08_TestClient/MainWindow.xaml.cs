d016a14471c47693aea08b5a2fe4642be5074c33
ARS1001 <SaidAroua@schleuniger.ch>
2020-04-03 10:14:35 +0200

    Added TestClient project, MVVM Layout included

---
+++ ACO08_TestClient/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ACO08_TestClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var model = new MainWindowModel();
+            var viewModel = new MainWindowViewModel(model);
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
---
+++ ACO08_TestClient/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACO08_TestClient
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowModel Model { get; }
+
+        public MainWindowViewModel(MainWindowModel model)
+        {
+            Model = model;
+        }
+    }
+}
