9440ed80aa08de688eb2bd6b703ad2694988b24c
Said Aroua <Said.Aroua@schleuniger.com>
2020-04-06 16:40:45 +0200

    Mockup Version 2 done

---
+++ ACO08_TestClient/Mockups/Version2/InterfaceMockupNr1V2.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ACO08_TestClient.Mockups.Version2
+{
+    /// <summary>
+    /// Interaction logic for InterfaceMockupNr1V2.xaml
+    /// </summary>
+    public partial class InterfaceMockupNr1V2 : Window
+    {
+        public InterfaceMockupNr1V2()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ ACO08_TestClient/Mockups/Version2/InterfaceMockupNr2V2.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ACO08_TestClient.Mockups.Version2
+{
+    /// <summary>
+    /// Interaction logic for InterfaceMockupNr2V2.xaml
+    /// </summary>
+    public partial class InterfaceMockupNr2V2 : Window
+    {
+        public InterfaceMockupNr2V2()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
